be8f6631dd543e13a4beac5f2c4b7c1092aa89a1
jamespeterson <jamespeterson0918@yahoo.com>
2017-09-28 16:34:54 -0400

    finished first index in theaters folder

---
+++ db/routes/theaterController.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const router = express.Router()
+
+const Schema = require("../db/schema.js");
+const TheaterModel = Schema.TheaterModel;
+
+
+router.get('/', (request, response) => {
+
+
+    TheaterModel.find({})
+    .then((theaters) => {
+        response.render('companies/index', {
+            theaters: theaters
+        })
+    })
+    .catch((error) => {
+        console.log(error)
+    })
+})
